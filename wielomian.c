467d7be7b7687620d056761b8392ea162d78166b
LukaszKur <lukaszkurasz2@gmail.com>
2018-01-11 19:03:48 +0100

    SZERG TAYLORA

--- wielomian.c
+++ wielomian.c
@@ -3,19 +3,22 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
+#define DOKLADNOSC 50
 int main(int liczba_slow, char* tablica_slow[])
 {
-
     double x;
-    double wynik=0.0;
+    double wynik=1.0;
+    double silnia=1.0;
     double potega_x=1.0;
-    if(liczba_slow>2) { //pierwszy parametr to nazwa programu
+    if(liczba_slow==2) { //pierwszy parametr to nazwa programu
         x = atof(tablica_slow[1]);
-        for (int licznik = 2; licznik < liczba_slow; ++licznik) {
-            wynik += potega_x * atof(tablica_slow[licznik]);
+        for (int licznik = 1; licznik < DOKLADNOSC; ++licznik) {
             potega_x *= x;
+            silnia*=licznik;
+            wynik += potega_x /silnia;
+
         }
-        printf("Wielomian wynosi: %lf", wynik);
+        printf("Wynik: %lf", wynik);
     }else{
         printf("Nie poprawna liczba parametrow!\n");
     }
