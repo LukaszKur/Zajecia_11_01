0ea185f372ab713c8d0313763d86b9f34f4cf5ea
LukaszKur <lukaszkurasz2@gmail.com>
2018-01-12 10:22:52 +0100

    sortowanie

---
+++ scalanie_tablic_sort.c
@@ -0,0 +1,4 @@
+//
+// Created by lukasz on 12.01.18.
+//
+
--- sortowanie.c
+++ sortowanie.c
@@ -22,9 +22,9 @@ int main() {
     //posortuj
     for (int i = N-1; i >=0 ; --i)
     {
-        for (int j = 0; j <=i ; ++j)
+        for (int j = 1; j <=i ; ++j)
         {
-            if(tab[j-1]>tab[j])
+            if(tab[j-1] < tab[j])
             {
                 zamien(&tab[j-1],&tab[j]);
             }
